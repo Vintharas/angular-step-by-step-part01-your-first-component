173af41ec139e80d3834068836a88653fbabcd69
Jaime Gonzalez <jaime.gg83@gmail.com>
2020-12-11 22:59:35 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-step-by-step-part01-your-first-component';
+}
